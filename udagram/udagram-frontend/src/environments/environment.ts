a4f69d1fea8ef9dfacd2ff30bf25c03257b22335
MrGAFs <gogo172000@gmail.com>
2022-06-06 05:56:09 +0200

    Changing the ling

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://udagram-env.eba-fzpnppmg.us-east-1.elasticbeanstalk.com/'
+  apiHost: 'http://udagram-env.eba-qpmppyq2.us-east-1.elasticbeanstalk.com/api/v0',
 };
 
 /*
