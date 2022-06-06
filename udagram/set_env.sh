979bc8f2d920231495919a1ef9ca281d5c015d07
MrGAFs <gogo172000@gmail.com>
2022-06-06 16:47:45 +0200

    fixing bugs

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -1,11 +1,10 @@
-# This file is used for convenience of local development.
-# DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=postgres
-export POSTGRES_HOST=udagram-1.ciruikedhuqj.us-east-1.rds.amazonaws.com
-export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::udagram-buck
-export AWS_REGION=us-east-1
-export AWS_PROFILE=default
-export JWT_SECRET=mysecretstring
-export URL=http://localhost:8100
+#!/bin/sh
+export POSTGRES_USERNAME="postgres"
+export POSTGRES_PASSWORD="postgres"
+export POSTGRES_HOST="udagram-1.ciruikedhuqj.us-east-1.rds.amazonaws.com"
+export POSTGRES_DB="postgres"
+export AWS_BUCKET="udagram-buck"
+export AWS_REGION="us-east-1"
+export AWS_PROFILE="default"
+export JWT_SECRET="mysecretstring"
+export URL="http://localhost:8100"
