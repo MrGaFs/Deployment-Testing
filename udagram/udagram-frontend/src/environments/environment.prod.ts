746112706ac10609ea77c621d007a00e7729b5a9
MrGAFs <gogo172000@gmail.com>
2022-06-06 18:54:17 +0200

    .

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: 'Udagram',
-  apiHost: 'http://udagram-env.eba-qpmppyq2.us-east-1.elasticbeanstalk.com/api/v0',
+  apiHost: 'http://udagram-backend-env.eba-ws3gbpfk.us-east-1.elasticbeanstalk.com/api/v0',
 };
 
 /*
